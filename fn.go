03e3ff4ba874147eebc1845595769d4610af8bd8
roidaradal <johnroy.daradal@goodkredit.com>
2025-03-11 16:52:00 +0800

    v0.1.3 - Add All and Any

--- fn.go
+++ fn.go
@@ -32,3 +32,21 @@ func Lookup[T any, K comparable, V any](items []T, entry func(T) (K, V)) map[K]V
 	}
 	return lookup
 }
+
+func All[T any](items []T, ok func(T) bool) bool {
+	for _, item := range items {
+		if !ok(item) {
+			return false
+		}
+	}
+	return true
+}
+
+func Any[T any](items []T, ok func(T) bool) bool {
+	for _, item := range items {
+		if ok(item) {
+			return true
+		}
+	}
+	return false
+}
