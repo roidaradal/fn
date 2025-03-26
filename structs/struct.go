68a2afc25571b33e7886daffb2ae023bfc99bcdf
roidaradal <johnroy.daradal@goodkredit.com>
2025-03-26 14:41:18 +0800

    v0.1.9 - Add structs package

---
+++ structs/struct.go
@@ -0,0 +1,28 @@
+package structs
+
+import (
+	"encoding/json"
+	"reflect"
+
+	"github.com/roidaradal/fn/check"
+)
+
+func SetFieldValue(x any, field string, value any) {
+	if !check.IsStructPointer(x) {
+		return
+	}
+	reflect.ValueOf(x).Elem().FieldByName(field).Set(reflect.ValueOf(value))
+}
+
+func ToMap[T any, V any](item *T) (map[string]V, error) {
+	var output map[string]V
+	data, err := json.Marshal(item)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(data, &output)
+	if err != nil {
+		return nil, err
+	}
+	return output, nil
+}
