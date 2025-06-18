84081c52a58e7c1f7893eeffd649832cb0d88922
roidaradal <johnroy.daradal@goodkredit.com>
2025-06-18 09:11:29 +0800

    v0.2.1 - Graph Edges

--- ds/graph.go
+++ ds/graph.go
@@ -1,15 +1,19 @@
 package ds
 
+import "github.com/roidaradal/fn"
+
 type VertexPair = [2]string
 
 type Graph struct {
 	vertices    *Set[string]
+	edges       map[string][]string
 	edgeWeights map[VertexPair]int
 }
 
 func NewGraph() *Graph {
 	return &Graph{
 		vertices:    NewSet[string](),
+		edges:       make(map[string][]string),
 		edgeWeights: make(map[VertexPair]int),
 	}
 }
@@ -22,10 +26,26 @@ func (g Graph) EdgeWeight(pair VertexPair) int {
 	return g.edgeWeights[pair]
 }
 
+func (g Graph) Edges(v string) []string {
+	return g.edges[v]
+}
+
 func (g *Graph) AddVertex(v string) {
 	g.vertices.Add(v)
 }
 
 func (g *Graph) AddEdgeWeight(pair VertexPair, weight int) {
 	g.edgeWeights[pair] = weight
+}
+
+func (g *Graph) AddEdge(v1, v2 string) {
+	g.edges[v1] = append(g.edges[v1], v2)
+}
+
+func (g *Graph) InitEdges() {
+	for _, v := range g.Vertices() {
+		if !fn.HasKey(g.edges, v) {
+			g.edges[v] = make([]string, 0)
+		}
+	}
 }
