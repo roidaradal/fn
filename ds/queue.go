d9307a9ee69dffd40b57c097d837f47a2d13622e
roidaradal <johnroy.daradal@goodkredit.com>
2025-06-19 12:26:13 +0800

    v0.2.4 - Queue

---
+++ ds/queue.go
@@ -0,0 +1,33 @@
+package ds
+
+type Queue[T any] struct {
+	items []T
+}
+
+func NewQueue[T any]() *Queue[T] {
+	return &Queue[T]{
+		items: make([]T, 0),
+	}
+}
+
+func (q *Queue[T]) Enqueue(item T) {
+	q.items = append(q.items, item)
+}
+
+func (q *Queue[T]) Dequeue() T {
+	front := q.items[0]
+	q.items = q.items[1:]
+	return front
+}
+
+func (q Queue[T]) Front() T {
+	return q.items[0]
+}
+
+func (q Queue[T]) Len() int {
+	return len(q.items)
+}
+
+func (q Queue[T]) IsEmpty() bool {
+	return len(q.items) == 0
+}
