e18677cedd438868c7496d7799e3c0e40ff52895
roidaradal <johnroy.daradal@goodkredit.com>
2025-08-28 16:17:37 +0800

    v0.2.6 - IsEmpty & Deref

--- qol.go
+++ qol.go
@@ -7,3 +7,11 @@ func Ternary[T any](condition bool, valueTrue T, valueFalse T) T {
 		return valueFalse
 	}
 }
+
+func IsEmptyString(text string) bool {
+	return text == ""
+}
+
+func Deref[T any](item *T) T {
+	return *item
+}
