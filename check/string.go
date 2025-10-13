417eb0bc884006f45a0a3b0ef4290bf717332cd5
roidaradal <johnroy.daradal@goodkredit.com>
2025-10-13 16:18:49 +0800

    v0.4.7 - IsAlphaNumeric

--- check/string.go
+++ check/string.go
@@ -1,6 +1,11 @@
 package check
 
-import "net/mail"
+import (
+	"net/mail"
+	"regexp"
+)
+
+var alphaNumeric = regexp.MustCompile("^[a-zA-Z0-9]+$")
 
 // Check empty string, used for filter functions
 func IsEmptyString(text string) bool {
@@ -16,4 +21,9 @@ func NotEmptyString(text string) bool {
 func IsValidEmail(email string) bool {
 	emailAddress, err := mail.ParseAddress(email)
 	return err == nil && emailAddress.Address == email
+}
+
+// Checks if string is alphanumeric
+func IsAlphaNumeric(text string) bool {
+	return alphaNumeric.MatchString(text)
 }
