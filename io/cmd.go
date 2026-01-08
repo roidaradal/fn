4c912cfe3a59861e913d508014af713a50dd1296
roidaradal <johnroy.daradal@goodkredit.com>
2026-01-09 07:55:46 +0800

    v0.5.38 - io.SoftClearScreen

--- io/cmd.go
+++ io/cmd.go
@@ -1,11 +1,19 @@
 package io
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 // Clear screen
 func ClearScreen() {
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+// Soft Clear Screen
+func SoftClearScreen() {
 	fmt.Print("\033[2J\033[H")
-	// cmd := exec.Command("cmd", "/c", "cls")
-	// cmd.Stdout = os.Stdout
-	// cmd.Run()
 }
