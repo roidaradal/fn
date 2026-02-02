d8d5fc4fd6a9ab93d51aaee083b8ac3f414fc48f
roidaradal <johnroy.daradal@goodkredit.com>
2026-02-02 13:35:14 +0800

    v0.5.56 - SyncList

---
+++ list/sync.go
@@ -0,0 +1,56 @@
+package list
+
+import "sync"
+
+// Concurrency-safe generic list
+type SyncList[T any] struct {
+	mu    sync.RWMutex
+	items []T
+}
+
+// Create new SyncList
+func NewSyncList[T any]() *SyncList[T] {
+	return &SyncList[T]{items: make([]T, 0)}
+}
+
+// Create SyncList from existing list
+func SyncListFrom[T any](items []T) *SyncList[T] {
+	return &SyncList[T]{items: items}
+}
+
+// SyncList.Append
+func (sl *SyncList[T]) Append(item T) {
+	sl.mu.Lock()
+	defer sl.mu.Unlock()
+	sl.items = append(sl.items, item)
+}
+
+// SyncList.Clear
+func (sl *SyncList[T]) Clear() {
+	sl.mu.Lock()
+	defer sl.mu.Unlock()
+	clear(sl.items)
+}
+
+// SyncList number of items
+func (sl *SyncList[T]) Len() int {
+	sl.mu.RLock()
+	defer sl.mu.RUnlock()
+	return len(sl.items)
+}
+
+// SyncList underlying items
+func (sl *SyncList[T]) Items() []T {
+	sl.mu.Lock()
+	defer sl.mu.Unlock()
+	return Copy(sl.items)
+}
+
+// Copy SyncList's items and clear it
+func (sl *SyncList[T]) ClearItems() []T {
+	sl.mu.Lock()
+	defer sl.mu.Unlock()
+	items := Copy(sl.items)
+	clear(sl.items)
+	return items
+}
