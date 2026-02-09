c326425325d8ed187fc6872687ececbae2e31b32
roidaradal <johnroy.daradal@goodkredit.com>
2026-02-09 14:55:40 +0800

    v0.5.64 - Common Fail Errors

---
+++ fail/errors.go
@@ -0,0 +1,18 @@
+package fail
+
+import "errors"
+
+var (
+	InactiveItem    = errors.New("public: Inactive item")
+	InvalidCode     = errors.New("public: Invalid code")
+	InvalidDate     = errors.New("public: Invalid date")
+	InvalidField    = errors.New("public: Invalid field")
+	InvalidOption   = errors.New("public: Invalid option")
+	InvalidType     = errors.New("public: Invalid type")
+	MissingConfig   = errors.New("public: Missing config")
+	MissingField    = errors.New("public: Missing field")
+	MissingParams   = errors.New("public: Missing required parameters")
+	NotFoundAccount = errors.New("public: Account not found")
+	NotFoundItem    = errors.New("public: Item not found")
+	WrongType       = errors.New("public: Wrong type")
+)
