1b72fc5270c0befb2f957a54d9f6239b6dc38fb2
roidaradal <johnroy.daradal@goodkredit.com>
2026-03-03 15:42:09 +0800

    v0.5.70 - dict.GroupByFunc

--- dict/create.go
+++ dict/create.go
@@ -73,12 +73,35 @@ func GroupByValue[K, V comparable](items map[K]V) map[V][]K {
 	return groups
 }
 
+// Group data by values, using key and value transformers
+func GroupByFunc[K, B comparable, V, A any](items map[K]V, keyFn func(K) A, valueFn func(V) B) map[B][]A {
+	groups := make(map[B][]A)
+	for k, v := range items {
+		a, b := keyFn(k), valueFn(v)
+		groups[b] = append(groups[b], a)
+	}
+	return groups
+}
+
 // Group data (map[K][]V) by values => map[V][]K
 func GroupByValueList[K, V comparable](items map[K][]V) map[V][]K {
 	groups := make(map[V][]K)
 	for k := range items {
 		for _, v := range items[k] {
 			groups[v] = append(groups[v], k)
+		}
+	}
+	return groups
+}
+
+// Group data (map[K][]V) by values => map[V][]K, using key and value transformers
+func GroupByFuncList[K, B comparable, V, A any](items map[K][]V, keyFn func(K) A, valueFn func(V) B) map[B][]A {
+	groups := make(map[B][]A)
+	for k := range items {
+		a := keyFn(k)
+		for _, v := range items[k] {
+			b := valueFn(v)
+			groups[b] = append(groups[b], a)
 		}
 	}
 	return groups
